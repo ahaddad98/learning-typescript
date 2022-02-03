abbf44b8fbbf5caad79c3e142bdd21f41a629323
ahaddad98 <amine.haddadd@gmail.com>
2022-02-03 15:03:51 +0100

    learn middleware

--- crash-crash-course/app.js
+++ crash-crash-course/app.js
@@ -13,12 +13,14 @@ app.get('/', (req, res) => {
     res.render('index', {title: 'Home'});
 });
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'});
+    // res.sendFile('./views/about.html', {root: __dirname});
 });
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'create'});
+    // res.redirect('/about');
 });
 // 404 page
 app.use((req, res) => {
-    res.status().sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404', {title: '404'})
 });
